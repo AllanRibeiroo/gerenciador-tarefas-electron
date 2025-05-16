029cdc00630f3a202d1836072235c93e8039f669
Allan <teixeiraallan312@gmail.com>
2025-05-15 22:16:22 -0300

    fazendo mudanças no script

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,37 @@
 function addTask() {
-    
+    // Pega o input
     var input = document.getElementById("input-tarefa")
-    console.log(input.value)
-
-   // alert("Função de adicionar tarefa")
     
+
+    //alert("Função de adicionar tarefa")
+
+    // Pegar o valor do input
+    var inputValue = input.value;
+
+    // Limpar o input
+    input.value = "";
+
+    var list = document.getElementById("A-minha-Lista-de-tarefas")
+
+    // Criar um elemento li
+    var li = document.createElement("li")
+
+    // Adicionar o LI na lista de tarefas
+    list.appendChild(li)
+
+    // Adicionar o valor do INPUT para a LI
+    
+    // li.innerText = inputValue; 
+    // li.innerHTML = "A tarefa <button>Excluir</button>";
+    // li.innerHTML = inputValue + "<button>Excluir</button>"
+    li.innerHTML = ` ${inputValue}<button onclick = "this.parentElement.remove()" >🗑️</button> `;
+    li.onclick = function() {
+        li.className = "tarefa-completa";
+    }
+
+
+    console.log(inputValue)
 
 }
+
+
